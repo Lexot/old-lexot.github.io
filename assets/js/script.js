0ebff9313b7d09871b15b04d6e9f165e646e127f
Lexot <alexa.morenohrn@uanl.edu.mx>
2018-09-06 02:42:23 +0100

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,3 @@
 $(document).ready(function() {
-  $(".about").click(function() {
-    $("html,body").animate(
-      {
-        scrollTop: $(".banner").offset().top
-      },
-      "slow"
-    );
-  });
-
-  $(".portfolio-link").click(function() {
-    $("html,body").animate(
-      {
-        scrollTop: $(".portfolio").offset().top
-      },
-      "slow"
-    );
-  });
-
-  $(".contact-link").click(function() {
-    $("html,body").animate(
-      {
-        scrollTop: $(".contact").offset().top
-      },
-      "slow"
-    );
-  });
+  AOS.init();
 });
