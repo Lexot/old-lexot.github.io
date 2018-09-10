0ad761151cfa8c6a09ff01f71f4da63e61d1df4d
Lexot <alexa.morenohrn@uanl.edu.mx>
2018-09-10 18:01:04 -0500

    works page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
 $(document).ready(function() {
-  AOS.init();
+  //AOS.init();
+  $(".ti-1").hover(
+    function() {
+      $(".img-1").fadeIn();
+    },
+    function() {
+      $(".img-1").hide();
+    }
+  );
+  $(".ti-2").hover(
+    function() {
+      $(".img-2").fadeIn();
+    },
+    function() {
+      $(".img-2").hide();
+    }
+  );
+  $(".ti-3").hover(
+    function() {
+      $(".img-3").fadeIn();
+    },
+    function() {
+      $(".img-3").hide();
+    }
+  );
+  $(".ti-4").hover(
+    function() {
+      $(".img-4").fadeIn();
+    },
+    function() {
+      $(".img-4").hide();
+    }
+  );
+
+  $(".ti-1b").click(function() {
+    toggleImage(1);
+  });
+  $(".ti-2b").click(function() {
+    toggleImage(2);
+  });
+  $(".ti-3b").click(function() {
+    toggleImage(3);
+  });
+  $(".ti-4b").click(function() {
+    toggleImage(4);
+  });
 });
+
+function toggleImage(index) {
+  for (let i = 0; i < 4; i++) {
+    if (index !== i + 1) $(".img-" + (i + 1)).removeClass("show");
+    else $(".img-" + (i + 1)).toggleClass("show");
+  }
+}
